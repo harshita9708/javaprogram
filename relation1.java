e61bc9500e1240e434ff189e179367432efb0d01
harshita9708 <harshitahosamani04@gmail.com>
2025-04-11 10:32:11 +0530

    save

---
+++ relation1.java
@@ -0,0 +1,20 @@
+public class relation1{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        if(a>b){
+        System.out.println("A is greater than B "+(a>b));
+        }
+        else if(a<b){
+        System.out.println("B is greater than A "+(a<b));
+        }
+        else if(a==b){
+        System.out.println("A is equal to  B "+(a==b));
+        }
+        else{
+        System.out.println("A is not equal to B "+(a!=b));
+        }
+        System.out.println("helloworld");
+    }
+}
