45aabbcfbfe5ed7b1993ebaeb841a527946d7a6e
harshita9708 <harshitahosamani04@gmail.com>
2025-04-11 09:33:35 +0530

    save classobject file

---
+++ classobeject.java
@@ -0,0 +1,14 @@
+public class classobeject{
+    public int add(int a, int b)
+    {
+        System.out.println("Addition result: "+(a+b));
+
+    }
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b= 20;
+        classobeject obj =new classobeject();
+        obj.add(a,b);
+    }
+} 
